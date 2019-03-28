94d8917772f3aaac4b574936bcb312a5a7e190a3
Joshua White <joshua.jj.white@gmail.com>
2019-03-28 11:02:44 -0400

    play with images and card fields

--- flunger/src/App.js
+++ flunger/src/App.js
@@ -8,28 +8,44 @@ const mtg = require('mtgsdk');
 
 class App extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
+  }
+
   componentDidMount() {
-    mtg.card.where({subtypes: 'spider' })
-      .then(cards =>
-         cards.forEach(card => console.log(card.name)))
+    mtg.card.where(
+      {name: 'invent'}
+      // {supertypes: 'Legendary', subtypes: 'spider' }
+      // {name: 'Deadly', subtypes: 'spider'}
+      )
+      .then(cards => {
+        this.setState({cards: cards})
+        cards.forEach(card => console.log(card.name))
+      })
   }
   
   render() {
+
+    const {cards} = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {cards
+          .map(card => {
+            return (
+              <div>
+                <p>{card.names} {card.manaCost} ({card.cmc})</p>
+                <p>{card.number}</p>
+                <p>{card.type} ({card.power}/{card.toughness})</p>
+                <img src={card.imageUrl} />
+              </div>
+            )
+          })}
         </header>
       </div>
     );
